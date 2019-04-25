d3d2b1ae057c0b513867fd6260da37f26cd5925e
alete89 <maildealete@gmail.com>
2019-04-25 15:14:13 -0300

    prueba de terminal

---
+++ term.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+from PyQt4.QtCore import QProcess
+from PyQt4.QtGui import QWidget, QVBoxLayout, QPushButton, QApplication, QLineEdit
+
+
+class embeddedTerminal(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        self._processes = []
+        self.resize(800, 600)
+        self.terminal = QWidget(self)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.terminal)
+        self._start_process('xterm',
+                            ['-into', str(self.terminal.winId()), '-e',
+                             'tmux', 'new', '-s', 'ale']
+                            )
+        self.textBox = QLineEdit(self)
+        self.button = QPushButton('run-in-terminal')
+        layout.addWidget(self.textBox)
+        layout.addWidget(self.button)
+        self.button.clicked.connect(self.run_command)
+
+    def _start_process(self, prog, args):
+        child = QProcess()
+        self._processes.append(child)
+        child.start(prog, args)
+
+    def run_command(self):
+        command = self.textBox.text()
+        self._start_process('tmux', ['send-keys', '-t', 'ale:0', command, 'Enter'])
+
+    def closeEvent(self, event):
+        self.kill_tmux()
+        event.accept()
+
+    def kill_tmux(self):
+        self._start_process('tmux', ['kill-session', '-t', 'ale'])
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = embeddedTerminal()
+    main.show()
+    sys.exit(app.exec_())
